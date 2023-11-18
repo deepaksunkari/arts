14be68a7a486579f5d421a1098288db3911ceda8
deepaksunkari <89195168+deepaksunkari@users.noreply.github.com>
2023-11-18 19:55:07 +0530

    Update actiongroup.ps1

--- actiongroup.ps1
+++ actiongroup.ps1
@@ -1,27 +1,56 @@
 Connect-AzAccount
 
-$subcriptionID= Read-Host -Prompt "Input your subscription name/ID"
+# Ask the user for input
+$subscriptionId = Read-Host -Prompt "Input your subscription name/ID"
+$resourceGroupName = "ARTS-SubBudget"
+$location = "Central India"
+$locationActionGroup = "Global"
+$countryCode= "91"
+$contactNumber = Read-Host -Prompt "Input your contact number (Example: 987654321)"
+$actionGroupName = "SubAlert"
+$actionGroupShortName = "SubAlert"
+$emailId = "dsunkari@microsoft.com"
+$emailalertname= "SubBudgetAlert"
+$smsalertname= "SubBudgetSMS"
 
-$resourceGroupName= Read-Host -Prompt "Input the Resource Group name where you will create action group in few minutes"
-$location= Read-Host -Prompt "Input the location for the Resource Group"
-$actionGroupName= Read-Host -Prompt "Input a name less than 12 characters using lowercase for action group"
-$actionshortName= Read-Host -Prompt "Input a name less than 12 characters using lowercase for action group display name"
-$emailalertname= Read-Host -Prompt "Input a display name for email notification alert"
-$smsalertname= Read-Host -Prompt "Input a display name for sms notification alert"
-$emailID= Read-Host -Prompt "Input your email and managers ID as example: dsunkari@microsoft.com"
-$countryCode= Read-Host -Prompt "Input your mobile country code. ex: 91"
-$contactnumber= Read-Host -Prompt "Input your contact number"
+# Connect to your Azure account
+Connect-AzAccount -Subscription $subscriptionId
 
-select-AzSubscription -Subscription "$subcriptionID"
+# Check if the resource group exists, and create it if not
+if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+    $createdResourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+    if ($createdResourceGroup) {
+        Write-Output "Resource group '$resourceGroupName' created in location '$location'."
+    } else {
+        Write-Output "Failed to create resource group '$resourceGroupName'."
+    }
+} else {
+    Write-Output "Resource group '$resourceGroupName' already exists."
+}
 
-$email = New-AzActionGroupReceiver -Name $emailalertname -EmailReceiver -EmailAddress $emailID
+$email = New-AzActionGroupEmailReceiverObject -Name $emailalertname -EmailAddress $emailID
 
-$sms = New-AzActionGroupReceiver -Name $smsalertname -SmsReceiver -CountryCode $countryCode -PhoneNumber $contactnumber
+$sms = New-AzActionGroupSmsReceiverObject -Name $smsalertname -CountryCode $countryCode -PhoneNumber $contactNumber
 
-Set-AzActionGroup -ResourceGroupName $resourceGroupName -Name $actionGroupName -ShortName $actionshortName -Receiver $email, $sms -Debug
+# Define the action group properties
+$actionGroupEmailProperties = @{
+    "GroupShortName" = $actionGroupShortName
+    "EmailReceiver" = $email
+}
 
-$ActionGroupId= (Get-AzActionGroup -ResourceGroupName $resourceGroupName -Name $actionGroupName).Id
-Write-Host "$ActionGroupId"
-#Set-AzureRmActionGroup -Name $actionGroupName -ResourceGroupName $resourceGroupName -ShortName $actionshortName -Receiver $email,$sms
+$actionGroupSmsProperties = @{
+    "GroupShortName" = $actionGroupShortName
+    "ContactReceiver" = $sms
+}
+
+# Create the Azure Action Group
+$actionGroup = New-AzActionGroup -Location $locationActionGroup -ResourceGroupName $resourceGroupName -Name $actionGroupName -ShortName $actionGroupShortName -EmailReceiver $email -SmsReceiver $sms -Debug
+
+if ($actionGroup) {
+    Write-Output "Azure Action Group '$actionGroupName' created in resource group '$resourceGroupName'."
+    Write-Output "Action Group Resource ID: $($actionGroup.Id)"
+} else {
+    Write-Output "Failed to create Azure Action Group '$actionGroupName' in resource group '$resourceGroupName'."
+}
